efe600c977e8da38f2889471605c8b7c0535c185
Logan Leonardson <logan.leonardson@gmail.com>
2018-07-15 23:08:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,16 @@ function shout(string) {
     console.log (string.toLowerCase())
   }
   function sayHiToGrandma(string) {
-    if (lowercase)
+    if (lowercase) {
+      return "I can't hear you" }
+      else if (uppercase) {
+        return "YES INDEED!" }
+        else (mixedCase) {
+          return "I love you, too!"
+        }
+      }
+    }
+    
+    var lowercase = .string
+    var uppercase = .string
+    var mixedCase = .string
